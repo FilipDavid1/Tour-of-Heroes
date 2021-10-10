60f705b7ab0de00760233e9eef7d3587204cd117
FilipDavid1 <filipenkodavid@gmail.com>
2021-10-10 11:03:21 +0200

    add heroes and items lists

---
+++ src/app/mock-items.ts
@@ -0,0 +1,14 @@
+import { Item } from "./item";
+
+export const ITEMS: Item[] = [
+    {id: 11, name: 'Combat Knife', price: 100},
+    {id: 12, name: 'Helmet', price: 210},
+    {id: 13, name: 'Leather Boots', price: 85},
+    {id: 14, name: 'Hunter Knife', price: 320},
+    {id: 15, name: 'Curtal-axe', price: 500},
+    {id: 16, name: 'Wooden Shield', price: 230},
+    {id: 17, name: 'Long Bow', price: 1000},
+    {id: 18, name: 'Katana', price: 2050},
+    {id: 19, name: 'Magic Wand', price: 10000},
+    {id: 20, name: 'Gloves of Strange Words', price: 50000},
+]
