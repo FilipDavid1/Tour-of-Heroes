f24cee65648466274e959d17feb8f707d0788f0f
FilipDavid1 <filipenkodavid@gmail.com>
2021-11-30 08:16:02 +0000

    add InMemoryData and http in hero.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { FormsModule } from '@angular/forms';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,14 +25,21 @@ import { FreeItemsComponent } from './free-items/free-items.component';
     ItemDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    FreeItemsComponent
+    FreeItemsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    FormsModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
